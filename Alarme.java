d1d5901606c6293ec324a9a6e25c66da4b0fa1d1
AXDOOMER <alexandrexavier@live.ca>
2017-06-08 02:00:28 -0400

    améliorations

--- Alarme.java
+++ Alarme.java
@@ -3,24 +3,38 @@
 // Ce programme n'est pas programmé très rigoureusement.
 // Je ne suis pas responsable des gens qui ne se réveillent pas.
 
-// IMPORTS
-import javax.swing.*;
+// Interface graphique
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 import java.awt.GridLayout;
+
+// Fichiers
 import java.io.File;
 
+// Numeric boxes
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+
+// Date / Time
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// Sound
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 import javafx.embed.swing.JFXPanel;
 
 // Classe d'un thread qui roule dans le fond
-class Veille implements Runnable {  
+class Veille implements Runnable {
+
+	int tics = 0;
+	final int THRES = 3;
 
 	public Veille(SpinnerModel h, SpinnerModel m) {
-		// Doit être initialisé manuellement pour que le son fonctionne
+		// Doit être initialisé pour que le son fonctionne
 		final JFXPanel fxPanel = new JFXPanel();
 
 		// Beurk!
@@ -32,11 +46,10 @@ class Veille implements Runnable {
 	SpinnerModel heures;
 	SpinnerModel minutes;
 
-	public void run() {  
-		boolean buzzing = false;
-		while(!buzzing) {
+	public void run() {
+		while(true) {
 			try {
-				Thread.sleep(2*1000);
+				Thread.sleep(1*1000);
 			} catch(InterruptedException ie) {
 				Thread.currentThread().interrupt();
 				System.err.println(ie);
@@ -50,10 +63,15 @@ class Veille implements Runnable {
 			// C'est tout un optimisation
 			if (time.getMinute() == wake.getMinute()) {
 				if (time.getHour() == wake.getHour()) {
-					buzzer();
-					buzzing = true;
+					if (++tics > THRES) {
+						buzzer();
+						break;	// Sort de cette boucle pour ne pas buzzer plusieurs fois
+					}
+					continue;	// On fait revérifier quelques autres fois le temps
 				}
 			}
+
+			tics = 0;
 		}
 
 		while(true) {
@@ -76,12 +94,12 @@ class Veille implements Runnable {
 		});
 		a.play();
 	}
-}  
+}
 
 // Classe princiaple (GUI)
 public class Alarme extends JFrame {
 	public Alarme() {
-		// Make a panel with a grid layout
+		// Faire un paneau avec une disposition en grille
 		GridLayout layout = new GridLayout(2,2);
 		JPanel pan = new JPanel();
 		pan.setLayout(layout);
@@ -90,7 +108,7 @@ public class Alarme extends JFrame {
 		JLabel lbl1 = new JLabel("Entrez heure:");
 		pan.add(lbl1);
 
-		// 2
+		// 2 - il est à noter qu'un spinner ne met pas à jour sa valeur tant que le focus n'as pas changé
 		SpinnerModel heures = new SpinnerNumberModel(8, 0, 23, 1);
 		JSpinner heuresBox = new JSpinner(heures);
 		pan.add(heuresBox);
@@ -99,7 +117,7 @@ public class Alarme extends JFrame {
 		JLabel lbl3 = new JLabel("Entrez minutes:");
 		pan.add(lbl3);
 
-		// 4
+		// 4 - il est à noter qu'un spinner ne met pas à jour sa valeur tant que le focus n'as pas changé
 		SpinnerModel minutes = new SpinnerNumberModel(30, 0, 59, 1);
 		JSpinner minutesBox = new JSpinner(minutes);
 		pan.add(minutesBox);
@@ -109,7 +127,7 @@ public class Alarme extends JFrame {
 		this.pack();
 		this.setVisible(true);
 
-		// Muh
+		// Muh threads
 		Veille v = new Veille(heures, minutes);
 		Thread t1 = new Thread(v);
 		t1.start();
